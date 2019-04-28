9bba910891408b5d2eae02fec082216cf764d18c
Walter Martins <wdmartins@gmail.com>
2019-04-28 19:18:20 -0400

    Added OTA Update Handling

--- pumpsControl/pumpsControl.ino
+++ pumpsControl/pumpsControl.ino
@@ -1,8 +1,9 @@
 #include <WiFiManager.h>
+#include <ESP8266WebServer.h>
+#include <ArduinoOTA.h>
 #include <Ticker.h>
 #include <LongTicker.h>
 #include <StatusLED.h>
-#include <ESP8266WebServer.h>
 #include <bitset>
 #include <time.h>
 
@@ -541,6 +542,30 @@ void setup() {
   // Start http server
   startServer();
 
+  // Initialize OTA (Over the air) update
+  ArduinoOTA.setHostname("ESP8266");
+  ArduinoOTA.setPassword("esp8266");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("OTA ready");
+
   statusLed.setStatus(StatusLED::Status::stable);
 }
 
@@ -549,8 +574,8 @@ void setup() {
 /*------------------------------------------------------------------------------------*/
 void loop() {
   server.handleClient();
+  ArduinoOTA.handle();
   // Check switches
-
   int poolSwitch = digitalRead(POOL_SWITCH);
   if (poolSwitch != currentPoolSwitchStatus) {
     currentPoolSwitchStatus = poolSwitch;
